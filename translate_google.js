95def63d84f4b127aef57fe34aede8ac4f57752e
itto4869 <itto4869@gmail.com>
2024-10-01 11:55:40 +0900

    google translate

---
+++ translate_google.js
@@ -0,0 +1,14 @@
+let text = "吾輩は猫である"
+let fromLang = "ja"
+let toLang = "en"
+
+const URL = "https://translation.googleapis.com/language/translate/v2?key=" + apikey + 
+            "&q=" + encodeURI(text) + "&source=" + fromLang + "&target=" + toLang;
+
+let xhr = new XMLHttpRequest();
+xhr.open("POST", [URL], false);
+xhr.send();
+if (xhr.status == 200) {
+    let res = JSON.parse(xhr.responseText);
+    console.log(res.data.translations[0].translatedText);
+}
