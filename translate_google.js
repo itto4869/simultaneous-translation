9755b0ebaaa15d570f12e4caf36906fd6591bb47
itto4869 <itto4869@gmail.com>
2024-10-08 11:33:02 +0900

    Add .gitignore file and update translate_google.js
    - Add .gitignore file to ignore api_key.js
    - Update translate_google.js to include a function for translating text using the Google Translate API

--- translate_google.js
+++ translate_google.js
@@ -1,14 +1,30 @@
-let text = document.getElementById('Pre-translation');
-let fromLang = "ja"
-let toLang = "en"
+function translateText() {
+    let textElement = document.getElementById('Pre-translation');
+    let text = textElement.value;
+    let fromLang = "ja";
+    let toLang = "en";
 
-const URL = "https://translation.googleapis.com/language/translate/v2?key=" + apikey + 
-            "&q=" + encodeURI(text) + "&source=" + fromLang + "&target=" + toLang;
+    const URL = "https://translation.googleapis.com/language/translate/v2?key=" + apiKey;
+    
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", URL, true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    
+    let requestData = {
+        q: text,
+        source: fromLang,
+        target: toLang,
+        format: "text"
+    };
+    
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            let res = JSON.parse(xhr.responseText);
+            console.log(res.data.translations[0].translatedText);
+        } else if (xhr.readyState === 4) {
+            console.error("Error: " + xhr.status + " - " + xhr.responseText);
+        }
+    };
 
-let xhr = new XMLHttpRequest();
-xhr.open("POST", [URL], false);
-xhr.send();
-if (xhr.status == 200) {
-    let res = JSON.parse(xhr.responseText);
-    console.log(res.data.translations[0].translatedText);
+    xhr.send(JSON.stringify(requestData));
 }
