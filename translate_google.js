05bb50d5640a548b4746b6cf9598832271739ba7
なとりーむ <gen71ki05@icloud.com>
2024-10-08 15:35:25 +0900

    入力時に翻訳の呼び出しを追加

--- translate_google.js
+++ translate_google.js
@@ -1,26 +1,36 @@
-function translateText() {
-    let textElement = document.getElementById('Pre-translation');
-    let text = textElement.value;
+window.onload = function () {
+    let textArea = document.getElementById('Pre-translation-text');
+    textArea.addEventListener('keypress', () => {
+        let text_value = textArea.value;
+        console.log(text_value);
+        translateText(text_value);
+        });
+  };
+function translateText(text_value) {
+    console.log(text_value);
+    let text = text_value;
     let fromLang = "ja";
     let toLang = "en";
 
     const URL = "https://translation.googleapis.com/language/translate/v2?key=" + apiKey;
-    
+
     let xhr = new XMLHttpRequest();
     xhr.open("POST", URL, true);
     xhr.setRequestHeader("Content-Type", "application/json");
-    
+
+
     let requestData = {
         q: text,
         source: fromLang,
         target: toLang,
         format: "text"
     };
-    
+
+    console.log("翻訳前:" + text);
     xhr.onreadystatechange = function() {
         if (xhr.readyState === 4 && xhr.status === 200) {
             let res = JSON.parse(xhr.responseText);
-            console.log(res.data.translations[0].translatedText);
+            console.log("翻訳後:" + res.data.translations[0].translatedText);
         } else if (xhr.readyState === 4) {
             console.error("Error: " + xhr.status + " - " + xhr.responseText);
         }
