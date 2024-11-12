2f2d0e12c802a7e043a5d08dc3ba98e0aee877ca
itto4869 <itto4869@gmail.com>
2024-11-12 11:46:47 +0900

    feat: 翻訳ボタンを押されたら翻訳するように変更

--- display.js
+++ display.js
@@ -4,11 +4,12 @@ window.onload = function () {
     const textArea = document.getElementById("Pre-translation-text");
     const selectFromLang = document.getElementById("pre-translation-lang");
     const selectToLang = document.getElementById("post-translation-lang");
+    const button = document.getElementById("translate-button");
     const googleTextArea = document.getElementById("google-translate");
     const deeplTextArea = document.getElementById("deepL-translate");
     const LLMTextArea = document.getElementById("chatGPT-translate");
 
-    textArea.addEventListener("keypress", async () => {
+    button.addEventListener("click", async () => {
         try {
             const googleText = await translateGoogle(textArea.value, selectFromLang.value, selectToLang.value);
             const deeplText = await translateDeepL(textArea.value, deeplMap[selectFromLang.value], deeplMap[selectToLang.value]);
