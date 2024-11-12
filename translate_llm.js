fed79036edab06e9e6c517e42ec802c25d5eaad4
itto4869 <itto4869@gmail.com>
2024-11-12 11:11:31 +0900

    feat: add system instruction

--- translate_llm.js
+++ translate_llm.js
@@ -1,9 +1,14 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+const systemInstruction = "Translate the given text into the specified language. However, please output only the translated text."
+
 export async function translateLLM(textValue) {
     const genAI = new GoogleGenerativeAI(apiKeyGoogle);
     const toLang = "Japanese";
-    const model = genAI.getGenerativeModel({model: "gemini-1.5-pro" });
+    const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-pro",
+        systemInstruction: systemInstruction,
+    });
     const prompt = `Translate this text into ${toLang}: ${textValue}`;
     const result = await model.generateContent(prompt);
     return result.response.text();
